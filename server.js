8ba0d17c19b2a048c1fa9545a6cbf8da75480e67
fbuitron <franciscobuitron21@gmail.com>
2020-02-19 13:22:15 -0800

    added links that take you to API  information

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require('express')
 const fs = require('fs')
 const path = require('path')
-
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-app.use(express.static(path.json(__dirname, 'public')))
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/reservation', (req, res) => {
   fs.readFile('reservation.json', 'utf8', (e, data) => {
@@ -20,8 +19,7 @@ app.get('/waitlist', (request, response) =>{
   fs.readFile('waitlist.json', 'utf8', (error, data)=>{
     if(error){console.error(error)}
     const waitlist = JSON.parse(data)
-    console.log(data)
-    response.send(data)
+    response.json(waitlist)
   })
 })
 
